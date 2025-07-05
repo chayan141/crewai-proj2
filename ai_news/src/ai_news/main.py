af810a8bbf6fd3e970aec8743d920ab40ac07fdf
Chayan <chayan.paul.141@gmail.com>
2025-07-05 10:10:48 +0530

    ai_news_done

--- ai_news/src/ai_news/main.py
+++ ai_news/src/ai_news/main.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python
 import sys
 import warnings
-
 from datetime import datetime
-
-from ai_news.crew import AiNews
+from crew import AiNews
 
 warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
 
@@ -18,51 +16,27 @@ def run():
     Run the crew.
     """
     inputs = {
-        'topic': 'AI LLMs',
-        'current_year': str(datetime.now().year)
+        'topic': 'openai',
+        'date': datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
     }
-    
-    try:
-        AiNews().crew().kickoff(inputs=inputs)
-    except Exception as e:
-        raise Exception(f"An error occurred while running the crew: {e}")
+
+    inputs_array = [
+        {
+            'topic': 'ai agents',
+            'date': datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+        }, 
+        {
+            'topic': 'openai',
+            'date': datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+        },
+        {
+            'topic': 'hugging face',
+            'date': datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+        }
+    ]
+
+    # AiNews().crew().kickoff(inputs=inputs)
+    AiNews().crew().kickoff_for_each(inputs=inputs_array)
 
 
-def train():
-    """
-    Train the crew for a given number of iterations.
-    """
-    inputs = {
-        "topic": "AI LLMs",
-        'current_year': str(datetime.now().year)
-    }
-    try:
-        AiNews().crew().train(n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs)
-
-    except Exception as e:
-        raise Exception(f"An error occurred while training the crew: {e}")
-
-def replay():
-    """
-    Replay the crew execution from a specific task.
-    """
-    try:
-        AiNews().crew().replay(task_id=sys.argv[1])
-
-    except Exception as e:
-        raise Exception(f"An error occurred while replaying the crew: {e}")
-
-def test():
-    """
-    Test the crew execution and returns the results.
-    """
-    inputs = {
-        "topic": "AI LLMs",
-        "current_year": str(datetime.now().year)
-    }
-    
-    try:
-        AiNews().crew().test(n_iterations=int(sys.argv[1]), eval_llm=sys.argv[2], inputs=inputs)
-
-    except Exception as e:
-        raise Exception(f"An error occurred while testing the crew: {e}")
+run()
